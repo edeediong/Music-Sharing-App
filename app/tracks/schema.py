2e190c557e9d609730ed3b139f8ebbd34e2dd41f
edeediong <edidiongmichael08@gmail.com>
2020-04-01 20:25:45 +0100

    Initial Commit

---
+++ app/tracks/schema.py
@@ -0,0 +1,37 @@
+import graphene
+from graphene_django import DjangoObjectType
+from .models import Track
+
+class TrackType(DjangoObjectType):
+    """Class that tells Graphene Django of the structure and structure of our Track3 models."""
+    class Meta:
+        model = Track
+
+
+class Query(graphene.ObjectType):
+    tracks = graphene.List(TrackType)
+
+    def resolve_tracks(self, info):
+        return Track.objects.all()
+
+
+class CreateTrack(graphene.Mutation):
+    track = graphene.Field(TrackType)
+
+    class Arguments:
+        title = graphene.String()
+        description = graphene.String()
+        url = graphene.String()
+
+    def mutate(self, info, title, description, url):
+        track = Track(title=title, description=description, url=url)
+        track.save()
+        return CreateTrack(track=track)
+
+    # def mutate(self, info, **kwargs):
+    #     kwargs.get('title')
+    #     kwargs.get('description')
+    #     kwargs.get('url')
+
+class Mutation(graphene.ObjectType):
+    create_track = CreateTrack.Field()
